e5dff95bd79b96590df8a7c74b8ba5ba9f512245
Molly Obermeier <obermeier.mol@gmail.com>
2022-03-31 07:04:56 -0600

    About page updates

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,7 +6,11 @@ const About = () => {
         <h3> BOOK CLUBBIN </h3>
         <h5>Welcome to the book club! 
           Add your favorite books, keep track of if you've read them or not, develop a full library! </h5>
+          <a href="https://github.com/vinhnghiemcr/FoodTrucks">
+            Molly Obermeier
+          </a>
     </div>
+    
   )
 }
 
